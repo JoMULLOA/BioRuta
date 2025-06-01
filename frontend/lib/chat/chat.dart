126ade70156f126ec674e61306d6691f6a18e57d
Francisco Cisterna <francisco.cisterna2101@alumnos.ubiobio.cl>
2025-05-31 21:58:27 -0400

    fix chat page
    se arregla el error de que no se mostrar la navbar al abrir la pagina del chat

--- frontend/lib/chat/chat.dart
+++ frontend/lib/chat/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:BioRuta/navbar_widget.dart'; // Importa tu navbar
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -11,6 +12,18 @@ class ChatPage extends StatelessWidget {
       ),
       body: Center(
         child: const Text('Aquí va el contenido del chat'),
+      ),
+      // Aquí es donde agregamos la CustomNavbar
+      bottomNavigationBar: CustomNavbar(
+        currentIndex: 2, // Si quieres que el ícono de "Chat" esté seleccionado
+        onTap: (index) {
+          // Aquí manejas la lógica cuando se cambia de ítem en la navbar
+          if (index == 0) {
+            // Navegar a "Buscar"
+            Navigator.pushNamed(context, '/mapa');
+          }
+          // Puedes agregar lógica similar para otros índices de la navbar
+        },
       ),
     );
   }
